3973eb1c7054fdcafedc5efdb47e87af9f011b28
felixheptner <felix@heptner.de>
2025-09-22 18:35:58 +0200

    Fix default.nix and MOOCV in ts and sampler comp

--- slurm/default.nix
+++ slurm/default.nix
@@ -3,21 +3,30 @@ with import (builtins.fetchGit {
   rev = "35ad3c79b6c264aa73bd8e7ca1dd0ffb67bd73b1";
 }) { config = { allowUnfree = true; }; };
 
-
 mkShell {
   venvDir = "./_venv";
-  # Add dependencies that pip can't fetch here (or that we don't want to
-  # install using pip).
-  buildInputs = (with pkgs.python39Packages; [ python39 venvShellHook wheel ])
-     ++ (import ./system-dependencies.nix { inherit pkgs; });
+  venvPython = pkgs.python39.withPackages (ps: [ ps.pip ps.setuptools ps.wheel ]);
+
+  buildInputs =
+    (with pkgs.python39Packages; [ venvShellHook ])
+    ++ [
+      pkgs.gcc
+      pkgs.stdenv.cc.cc
+      pkgs.zlib
+    ]
+    ++ (import ./system-dependencies.nix { inherit pkgs; });
+
   postShellHook = ''
     unset SOURCE_DATE_EPOCH
-    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc]}:${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH"
-    export PYTHONPATH=$venvDir/${python39.sitePackages}:$PYTHONPATH
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc pkgs.zlib pkgs.gcc]}:$LD_LIBRARY_PATH"
+    export PYTHONPATH=$venvDir/${pkgs.python39.sitePackages}:$PYTHONPATH
   '';
+
   postVenvCreation = ''
     unset SOURCE_DATE_EPOCH
-    pip install -r requirements.txt
+    # Upgrade pip inside the venv
+    $venvDir/bin/python -m pip install --upgrade pip setuptools wheel
+    # Install project requirements (overwrite existing dirs automatically)
+    PIP_EXISTS_ACTION=w $venvDir/bin/pip install -r requirements.txt
   '';
 }
-
