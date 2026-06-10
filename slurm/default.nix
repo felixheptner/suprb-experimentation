b26805d6b4f9e563b38d43439c6ffaaa59c8af49
felixheptner <felix@heptner.de>
2026-06-10 13:26:25 +0200

    Change slurm script

--- slurm/default.nix
+++ slurm/default.nix
@@ -1,7 +1,8 @@
 with import (builtins.fetchGit {
   url = "https://github.com/NixOS/nixpkgs";
   # Pick a recent commit that includes Python 3.12
-  ref = "nixos-unstable";GIT
+  ref = "nixos-unstable";
+  # rev = "5df4d78a92474e6a45d8e6a9a77b6ca1f427c5c3"; # 2024-09-01
 }) { config.allowUnfree = true; };
 
 mkShell {
