4bbc56699f9374eda0aec3ba0d9219641edc88b2
felixheptner <felix@heptner.de>
2026-06-11 10:07:28 +0200

    Fix default.nix venv creation

--- slurm/default.nix
+++ slurm/default.nix
@@ -1,24 +1,27 @@
-nix
-with import (builtins.fetchGit {
-  url = "https://github.com/NixOS/nixpkgs";
-  ref = "nixos-unstable";
-}) { config.allowUnfree = true; };
+with import <nixpkgs> { config = { allowUnfree = true; }; };
+
 
 mkShell {
   venvDir = "./_venv";
-
+  # Add dependencies that pip can't fetch here (or that we don't want to
+  # install using pip).
   buildInputs = [
     pkgs.python312
+    pkgs.stdenv.cc.cc
   ] ++ (with pkgs.python312Packages; [
     venvShellHook
     wheel
     pip
-  ]) ++ (import ./system-dependencies.nix { inherit pkgs; });
-
+  ])
+     ++ (import ./system-dependencies.nix { inherit pkgs; });
   postShellHook = ''
     unset SOURCE_DATE_EPOCH
-    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc pkgs.zlib ]}:$LD_LIBRARY_PATH"
-    export PYTHONPATH=$venvDir/${pkgs.python312.sitePackages}:$PYTHONPATH
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [pkgs.stdenv.cc.cc]}:${pkgs.lib.makeLibraryPath [pkgs.zlib]}:$LD_LIBRARY_PATH"
+    export PYTHONPATH=$venvDir/${python312.sitePackages}:$PYTHONPATH
+  '';
+  postVenvCreation = ''
+    unset SOURCE_DATE_EPOCH
+    pip install --upgrade pip
     pip install -r requirements.txt
   '';
 }
